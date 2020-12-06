11dfa6ebf58e87cf73f53ba0e9f9078095ec0920
cadburry <72443203+cadburry6969@users.noreply.github.com>
2020-12-06 21:41:56 +0530

    Update cc.lua

--- cc.lua
+++ cc.lua
@@ -1,7 +1,7 @@
 Config                            = {}
 Config.Locale                     = 'en'
 
-Config.DrawDistance               = 100.0
+Config.DrawDistance               = 10.0
 Config.MaxInService               = -1
 Config.EnablePlayerManagement     = true
 Config.EnableSocietyOwnedVehicles = false
